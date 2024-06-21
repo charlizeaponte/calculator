92b1c8450a227559161552fe4b97e0a22d801414
Charlize Aponte <charlizeaponte@yahoo.com>
2024-06-21 17:15:41 +0000

    fixed

--- calc.py
+++ calc.py
@@ -1,6 +1,5 @@
 class Calc:
     """A simple calculator class"""
-
     def add(self, a, b):
         """Add two numbers together"""
         return a + b
@@ -8,20 +7,19 @@ class Calc:
     def sub(self, a, b):
         """Subtract two numbers"""
         return a - b
-    
+
     def mul(self, a, b):
         """Multiply two numbers"""
         return a * b
-    
+
     def div(self, a, b):
         """Divide two numbers"""
         return a / b
-    
+
     def pow(self, a, b):
         """Raise a to the power of b"""
         return a ** b
-    
+
     def sqrt(self, a):
         """Return the square root of a"""
         return a ** 0.5
-    
